776d7a0f7b7aa0a5bafd08646404ef82da7da22d
Iftekhar Rifat <rifat662@gmail.com>
2015-10-24 01:45:26 +0600

    ste_symbols: add compat symbol for GraphicBufferAllocator
    Change-Id: Ib93cc53bc9781e7f46ffb26f5d2bf2904efba9ab

--- libste_symbols/ste_camera.cpp
+++ libste_symbols/ste_camera.cpp
@@ -15,19 +15,37 @@
  */
 
 #include <system/window.h>
+#include <ui/PixelFormat.h>
 #include <ui/Rect.h>
 
 namespace android {
 
 // ---------------------------------------------------------------------------
 
-/* status_t GraphicBufferMapper::lock */
-extern "C" status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
-        buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr);
-extern "C" status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(
-        buffer_handle_t handle, int usage, const android::Rect& bounds, void** vaddr) {
-    return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
-            handle, (uint32_t)usage, bounds, vaddr);
+extern "C" {
+
+    /* status_t GraphicBufferMapper::lock */
+    status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
+            buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr);
+
+    status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(
+            buffer_handle_t handle, int usage, const android::Rect& bounds, void** vaddr) {
+        return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
+                handle, (uint32_t)usage, bounds, vaddr);
+    }
+
+    /* status_t GraphicBufferAllocator::alloc */
+    status_t _ZN7android22GraphicBufferAllocator5allocEjjijPPK13native_handlePj(
+            uint32_t width, uint32_t height, PixelFormat format, uint32_t usage,
+            buffer_handle_t* handle, uint32_t* stride);
+
+    status_t _ZN7android22GraphicBufferAllocator5allocEjjiiPPK13native_handlePi(
+            uint32_t width, uint32_t height, PixelFormat format, int usage,
+            buffer_handle_t* handle, uint32_t* stride) {
+        return _ZN7android22GraphicBufferAllocator5allocEjjijPPK13native_handlePj(
+                width, height, format, (uint32_t)usage, handle, stride);
+    }
+
 }
 
 // ---------------------------------------------------------------------------
